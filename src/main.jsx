a04d7f00b73e445347415751c8f9cbb2029035ad
adenadams20 <adamsdiene@gmail.com>
2026-01-23 09:44:44 +0000

    deploiment-3

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-const BASENAME = import.meta.env.VITE_BASENAME || "/RED-PRODUCT/";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/RED-PRODUCT/">
     <App />
     </BrowserRouter>
   </StrictMode>,
